16a68d087c9bf15c6dc37733e5fbc96d9283c9bb
jyshin <sjy1218vvv@gmail.com>
2020-11-18 15:18:43 +0900

    Hello added

---
+++ Hello/src/test/MainClass02.java
@@ -0,0 +1,12 @@
+package test;
+
+//파일명과 클래스명은 같아야한다.
+public class MainClass02 {
+	
+	//run했을 때 실행의 흐름이 시작되는 특별한 main메소드
+	public static void main(String[] args) {
+		System.out.println("하나");
+		System.out.println("두울");
+		System.out.println("세엣");
+	}
+}

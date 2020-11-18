b6450f98137aa495438e359133d83f4f51d20994
jyshin <sjy1218vvv@gmail.com>
2020-11-18 15:19:32 +0900

    Step02 added

---
+++ Step02_DataType/src/test/main/MainClass09.java
@@ -0,0 +1,24 @@
+package test.main;
+
+public class MainClass09 {
+	public static void main(String[] args) {
+		
+		//1.
+		//변수를 미리 만들어 두고 값을 나중에 대입해서 사용할거라면 기본값을 대입해 두는것이 좋다.
+		int num1=0;
+		
+		for(int i=0; i<10; i++) {
+			num1=num1+1;
+		}
+		
+		System.out.println(num1);
+		
+
+		//2.
+		//참조 데이터 type이 담길 수 있는 빈 공간은 null을 이용해서 만든다.
+		String str=null;
+		str = "어쩌구..저쩌구...";
+		
+		
+	}
+}
---
+++ Step02_DataType/src/test/main/MainClass10.java
@@ -0,0 +1,33 @@
+package test.main;
+
+public class MainClass10 {
+	public static void main(String[] args) {
+		
+		//기본 데이터 type
+		int num1=10;
+		double num2=10.1;
+		char ch1='a';
+		
+		//참조 데이터 type, str변수 안에는 참조값이 들어있다.
+		//str.을 통해 여러 기능들을 사용할 수 있다.
+		//str.을 통해 사용하는 기능들을 '메소드'라고 한다.
+		String str = "abcde12345";
+		int size = str.length();
+		char ch = str.charAt(5);
+		String result = str.toUpperCase(); //소문자를 모두 대문자로 변환한 문자열 얻어내기
+		
+		System.out.println(str);
+		System.out.println(ch);
+		System.out.println(size);
+		System.out.println(result);
+		
+		//result에도 '참조값'이 들어가있어 .(dot)을 통한 기능 사용이 가능하다.
+		System.out.println(result.charAt(0)); 
+		
+	
+		
+		
+		
+
+	}
+}
